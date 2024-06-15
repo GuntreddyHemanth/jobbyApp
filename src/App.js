314ad164d322ad9f9c5fac1469de56cf579d1687
GuntreddyHemanth <guntreddyhemanth@gmail.com>
2024-06-15 09:54:45 +0000

    save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import LoginForm from './components/LoginForm'
 import ProtectedRoute from './components/ProtectedRoute'
 import Home from './components/Home'
 import AllJobs from './components/AllJobs'
+import NotFound from './components/NotFound'
+import AboutJobItem from './components/AboutJobItem'
 
 import './App.css'
 
@@ -11,6 +13,9 @@ const App = () => (
     <Route exact path="/login" component={LoginForm} />
     <ProtectedRoute exact path="/" component={Home} />
     <ProtectedRoute exact path="/jobs" component={AllJobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={AboutJobItem} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
   </Switch>
 )
 
